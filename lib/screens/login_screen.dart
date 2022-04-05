0942efaece07af264ca2dadee0f0c9bb484046f8
Thiago Nagaoka <nagaoka.thiago@bol.com.br>
2022-04-05 14:20:06 -0300

    Created Login screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,71 @@
+import 'package:convida_ai_1/design/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class LoginWidget extends StatefulWidget {
+  const LoginWidget({Key? key}) : super(key: key);
+
+  @override
+  State<LoginWidget> createState() => _LoginWidgetState();
+}
+
+class _LoginWidgetState extends State<LoginWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+        child: Column(
+      children: [
+        const SizedBox(height: 120),
+        Image.asset('lib/assets/convida_logo.png', width: 317, height: 90),
+        const SizedBox(height: 170),
+        Container(
+            margin: const EdgeInsets.only(left: 25, right: 25, bottom: 20),
+            child: TextField(
+                decoration: InputDecoration(
+                    hintText: 'E-mail',
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide:
+                            BorderSide(color: AppColors.textFieldColor)),
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide:
+                            BorderSide(color: AppColors.textFieldColor))))),
+        Container(
+            margin: const EdgeInsets.only(left: 25, right: 25, bottom: 138),
+            child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    hintText: 'Password',
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide:
+                            BorderSide(color: AppColors.textFieldColor)),
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide:
+                            BorderSide(color: AppColors.textFieldColor))))),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+                margin: const EdgeInsets.only(right: 10),
+                child: SizedBox(
+                    width: 115,
+                    height: 55,
+                    child: ElevatedButton(
+                      style: Theme.of(context).elevatedButtonTheme.style,
+                      child: const Text('Login'),
+                      onPressed: () {},
+                    ))),
+            Container(
+                margin: const EdgeInsets.only(left: 10),
+                child: SizedBox(
+                    width: 115,
+                    height: 55,
+                    child: ElevatedButton(
+                        child: const Text('Registrar'),
+                        style: Theme.of(context).elevatedButtonTheme.style,
+                        onPressed: () {})))
+          ],
+        )
+      ],
+    ));
+  }
+}
+//Theme.of(context).appBarTheme.titleTextStyle,

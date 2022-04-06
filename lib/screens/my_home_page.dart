df4cf31abbf8fc9ab11e13ff66d02052110c7c91
Wilsley Germano <74660388+wilsleygermano@users.noreply.github.com>
2022-04-06 09:52:49 -0300

    updated main.dart (firebase login)

--- lib/screens/my_home_page.dart
+++ lib/screens/my_home_page.dart
@@ -1,11 +1,7 @@
 import 'package:convida_ai_1/screens/login_screen.dart';
-<<<<<<< Updated upstream
 
 import 'package:convida_ai_1/components/my_card.dart';
-=======
-import 'package:convida_ai_1/screens/my_home.dart';
 import 'package:firebase_auth/firebase_auth.dart';
->>>>>>> Stashed changes
 import 'package:flutter/material.dart';
 
 import '../components/my_sliver_app_bar.dart';
@@ -23,33 +19,29 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-<<<<<<< Updated upstream
-      body: CustomScrollView(
+      body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return CustomScrollView(
         slivers: <Widget>[
-          MySliverAppBar(),
+          const MySliverAppBar(),
           // deletar a partir daqui até o próximo comentário, pois este código serve apenas de teste
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return MyCard();
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                return const MyCard();
               },
               childCount: 20,
             ),
           ),
           // deletar até aqui para remover o código teste da sliverappbar
         ],
-      ),
-=======
-      body: StreamBuilder<User?>(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return const MyHome();
+      );
             } else {
               return const LoginWidget();
             }
           }),
->>>>>>> Stashed changes
     );
   }
 }

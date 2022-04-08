80f4fe0532e1b77bb4ceea60d0f878bd15bb116b
Thiago Nagaoka <nagaoka.thiago@bol.com.br>
2022-04-08 15:54:26 -0300

    functionality that takes events from firebase.

--- lib/screens/my_home_page.dart
+++ lib/screens/my_home_page.dart
@@ -1,21 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:convida_ai_1/components/my_drawer.dart';
 import 'package:convida_ai_1/screens/login_screen.dart';
 
 import 'package:convida_ai_1/components/my_card.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
+import '../classes/evento.dart';
 import '../components/my_sliver_app_bar.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
-
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var storage;
+  late List<Evento> eventsList;
+
+  @override
+  void initState() {
+    storage = FirebaseStorage.instance;
+    getEvents();
+    super.initState();
+  }
+
+  void getEvents() async {
+    eventsList = [];
+    FirebaseFirestore.instance
+        .collection('events')
+        .where('user_uid', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+        .get()
+        .then((snapshot) => snapshot.docs
+            .forEach((doc) => eventsList.add(Evento.fromJson(doc.data()))));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,22 +45,25 @@ class _MyHomePageState extends State<MyHomePage> {
       body: StreamBuilder<User?>(
           stream: FirebaseAuth.instance.authStateChanges(),
           builder: (context, snapshot) {
-            if (snapshot.hasData) {
+            if (snapshot.connectionState == ConnectionState.active) {
               return CustomScrollView(
-        slivers: <Widget>[
-          const MySliverAppBar(),
-          // deletar a partir daqui até o próximo comentário, pois este código serve apenas de teste
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-                return const MyCard();
-              },
-              childCount: 20,
-            ),
-          ),
-          // deletar até aqui para remover o código teste da sliverappbar
-        ],
-      );
+                slivers: <Widget>[
+                  const MySliverAppBar(),
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                        return MyCard(
+                          eventName: eventsList[index].titulo,
+                          eventDate: eventsList[index].data,
+                          eventLocation: eventsList[index].local,
+                          eventImage: NetworkImage(eventsList[index].url),
+                        );
+                      },
+                      childCount: eventsList.length,
+                    ),
+                  ),
+                ],
+              );
             } else {
               return const LoginWidget();
             }

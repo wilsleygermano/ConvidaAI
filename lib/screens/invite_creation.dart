783c5fa4fa2305d3815d699c93a9e225a001a096
Wilsley Germano <74660388+wilsleygermano@users.noreply.github.com>
2022-04-11 21:52:14 -0300

    FINALLY finished the image and text upload to fire store

--- lib/screens/invite_creation.dart
+++ lib/screens/invite_creation.dart
@@ -8,6 +8,7 @@ import 'package:convida_ai_1/controller/invite_creation_controller.dart';
 import 'package:convida_ai_1/design/app_colors.dart';
 import 'package:convida_ai_1/screens/my_home_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
@@ -33,6 +34,7 @@ class _InviteCreationState extends State<InviteCreation> {
   TextEditingController? eventCostController = TextEditingController();
   TextEditingController? eventPaymentController = TextEditingController();
   final userID = FirebaseAuth.instance.currentUser!.uid;
+  String? imageUrl;
 
   @override
   Widget build(BuildContext context) {
@@ -141,8 +143,14 @@ class _InviteCreationState extends State<InviteCreation> {
                     style: Theme.of(context).elevatedButtonTheme.style,
                     onPressed: () async {
                       if (file != null) {
-                        await _controller.uploadImage(file!, userID);
+                        await _controller.uploadImage(
+                            file!, eventNameController!.text);
                       }
+                      final url = await _controller
+                          .getImageUrl(eventNameController!.text);
+                      setState(() {
+                        imageUrl = url;
+                      });
                       await _controller.addTextFields(
                         eventNameController!.text,
                         eventDateController!.text,
@@ -150,6 +158,7 @@ class _InviteCreationState extends State<InviteCreation> {
                         eventCostController!.text,
                         eventPaymentController!.text,
                         userID,
+                        imageUrl,
                       );
                       showDialog(
                         context: context,
@@ -157,11 +166,10 @@ class _InviteCreationState extends State<InviteCreation> {
                           title: Text(
                             'Convite Criado!',
                             style: TextStyle(
-                              fontFamily: GoogleFonts.inter().fontFamily,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              color: AppColors.titlesColor
-                            ),
+                                fontFamily: GoogleFonts.inter().fontFamily,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: AppColors.titlesColor),
                           ),
                           content: Text(
                             'Agora é só esperar o grande dia',
@@ -184,10 +192,9 @@ class _InviteCreationState extends State<InviteCreation> {
                               child: Text(
                                 'Okay',
                                 style: TextStyle(
-                                  fontFamily: GoogleFonts.inter().fontFamily,
-                                  fontSize: 12,
-                                  color: AppColors.miniIconsColor
-                                ),
+                                    fontFamily: GoogleFonts.inter().fontFamily,
+                                    fontSize: 12,
+                                    color: AppColors.miniIconsColor),
                               ),
                             )
                           ],

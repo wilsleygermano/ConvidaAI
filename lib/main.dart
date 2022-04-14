11d7e5f36f81c2cc3840771541d02cc546970c77
Wilsley Germano <74660388+wilsleygermano@users.noreply.github.com>
2022-04-14 11:27:02 -0300

    trying to implement splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'screens/my_app.dart';
 
 void main() async {

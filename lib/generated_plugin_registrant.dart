eb4b3494b3d4533f7f8d2a73d6b2198a87a20964
Wilsley Germano <74660388+wilsleygermano@users.noreply.github.com>
2022-04-14 13:19:39 -0300

    added splash screen package

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -9,6 +9,7 @@ import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:firebase_storage_web/firebase_storage_web.dart';
+import 'package:flutter_native_splash/flutter_native_splash_web.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
 import 'package:share_plus_web/share_plus_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
@@ -21,6 +22,7 @@ void registerPlugins(Registrar registrar) {
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
   FirebaseStorageWeb.registerWith(registrar);
+  FlutterNativeSplashWeb.registerWith(registrar);
   ImagePickerPlugin.registerWith(registrar);
   SharePlusPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
